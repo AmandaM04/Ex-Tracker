d54154da607b74a592818d191e6ba7dc593dbf89
Amanda Mitchell <wreedamitchell@gmail.com>
2018-05-02 19:16:28 -0500

    created AJAX for locations and ex

---
+++ javascripts/locations.js
@@ -0,0 +1,8 @@
+// AJAX call for locations.json
+const loadLocations = (successFunction, errorFunction) => {
+  $.get('./db/locations.json')
+    .done(successFunction)
+    .fail(errorFunction);
+};
+
+module.exports = loadLocations;
