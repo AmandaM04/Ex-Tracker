4da4f0db92a97a720950cb755a24a527a17efa18
Amanda Mitchell <wreedamitchell@gmail.com>
2018-05-18 17:14:23 -0500

    search working

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,11 +1,12 @@
 // Writing to the dom
-const exOutputDiv = $('exSpot');
-const locationOutputDiv = $('exLocation');
+const exOutputDiv = $('#exSpot');
+const locationOutputDiv = $('#exLocation');
+// const mainOutputDiv = $('#main');
 
 const writeEx = (exes) => {
   exes.forEach((ex) => {
     let domStrang = '';
-    domStrang += `<div class="card">`;
+    domStrang += `<div class="card clickCard">`;
     domStrang += `<img class="img-rounded" src="${ex.image}" width="50%">`;
     domStrang += `<h1>${ex.name}</h1>`;
     domStrang += `<p>Age: ${ex.age}</p>`;
@@ -20,8 +21,8 @@ const writeLocations = (locations) => {
     let domString = '';
     domString += `<div class="locCard">`;
     domString += `<img src="${location.locationImage}" width="40%">`;
-    domString += `<p>${location.name}</p>`;
-    domString += `<p>${location.locationAddress}</p>`;
+    domString += `<p class="search">${location.name}</p>`;
+    domString += `<p class="search">${location.locationAddress}</p>`;
     domString += `<p>${location.timeThatExNormallyGoes}</p>`;
     domString += `</div>`;
     printToDom2(domString);
@@ -35,6 +36,28 @@ const printToDom = (exes) => {
 const printToDom2 = (locations) => {
   locationOutputDiv.append(locations);
 };
+
+// const singleEx = () => {
+//   let pup = {};
+//   return getAllPups().then((pups) => {
+//     pup = pups[0];
+//     return Promise.all([firstFoodJSON(), secondFoodJSON(), thirdFoodJSON(),]);
+//   }).then((foodz) => {
+//     const allTheFood = [...foodz[0], ...foodz[1], ...foodz[2],];
+//     const matching = allTheFood.filter((food) => {
+//       if (pup.favFoodId === food.key) {
+//         return true;
+//       };
+//       return false;
+//     });
+//     pup.favFood = matching;
+//     return Promise.resolve(pup);
+//   });
+// };
+
+// const printToDom3 = (singleEx) => {
+//   mainOutputDiv.html(singleEx);
+// };
 
 module.exports = {
   writeEx,
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,15 +1,57 @@
 // Attaching all event listeners
-$(':button').click(function () {
-  if (this.id === 'Morning') {
-    console.log('Morning clicked');
-  }
-  if (this.id === 'Afternoon') {
-    console.log('Afternoon clicked');
-  }
-  if (this.id === 'Evening') {
-    console.log('Evening clicked');
-  }
-  else if (this.id === 'After Dark') {
-    console.log('After Dark clicked');
-  }
-});
+// $(':button').click(function () {
+//   if (this.id === 'Morning') {
+//     console.log('Morning clicked');
+//   }
+//   if (this.id === 'Afternoon') {
+//     console.log('Afternoon clicked');
+//   }
+//   if (this.id === 'Evening') {
+//     console.log('Evening clicked');
+//   }
+//   else if (this.id === 'After Dark') {
+//     console.log('After Dark clicked');
+//   }
+// });
+
+// const filterLocations = () => {
+//   $('btn').on('click', () => {
+//     $('#locateMe .location-card').show();
+//     const button = $(event.target).html();
+//     if (button === 'Morning') {
+//       $('#locateMe').not('.Morning').toggle();
+//     } else if (button === 'Afternoon') {
+//       $('#locateMe .location-card').not('.Afternoon').toggle();
+//     } else if (button === 'Evening') {
+//       $('#locateMe .location-card').not('.Evening').toggle();
+//     } else if (button === 'After-Dark') {
+//       $('#locateMe .location-card').not('.After-Dark').toggle();
+//     }
+//   });
+// };
+
+// jQuery.expr[':'].iContains = function (a, i, m) {
+//   return jQuery(a).text().toUpperCase()
+//     .indexOf(m[3].toUpperCase()) >= 0;
+// };
+
+// const letsSearch = () => {
+//   $('#search').keypress((event) => {
+//     if (event.which === 13) {
+//       const userInput = $('#search').val();
+//       $(`#exLocation .locCard:not(:iContains(${userInput}))`).hide();
+//     }
+//   });
+// };
+
+// $('.clickCard').click((e) => {
+//   /* data.singlePup is a function that returns a promise */
+//   alert($('.clickCard').index(this));
+//   // data.singlePup().then((pup) => {
+//   //   dom.printPup(pup);
+//   // });
+// });
+
+module.exports = {
+  // letsSearch,
+};
